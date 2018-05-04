aca36f0c0d7bb139d26d9fbf59a1979112d981d2
Your Name <example@example.com>
2018-05-04 10:41:56 +0100

    commit3

--- app/src/main/java/com/productions/esaf/cafe/MainActivity.java
+++ app/src/main/java/com/productions/esaf/cafe/MainActivity.java
@@ -27,11 +27,8 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     //Variaveis
-    private Button btnChooseImage,btnUploadImage,btnLogin,btnSignUp;
+    private Button btnLogin,btnSignUp;
     private ImageView imageView;
-
-    private Uri filePath;
-    private final int PICK_IMAGE_REQUEST=71;
 
     //Firebase
     FirebaseStorage storage;
@@ -47,24 +44,11 @@ public class MainActivity extends AppCompatActivity {
         mStorageRef=storage.getReference();
 
         //Init View
-        btnChooseImage= findViewById(R.id.btnChooseImage);
-        btnUploadImage=findViewById(R.id.btnUploadImage);
         btnLogin=findViewById(R.id.btnLogin);
         btnSignUp=findViewById(R.id.btnSignUp);
         imageView=findViewById(R.id.imageView);
 
-        btnChooseImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chooseImage();
-            }
-        });
-        btnUploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage();
-            }
-        });
+
         //Botao Login
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,65 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    private void uploadImage() {
-        if(filePath !=null)
-        {
-            final ProgressDialog progressDialog =new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            StorageReference ref= mStorageRef.child("images/"+ UUID.randomUUID().toString());
-            ref.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.dismiss();
-                            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressDialog.dismiss();
-                            Toast.makeText(MainActivity.this, "Failed to Upload"+ e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress=(100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
-                            progressDialog.setMessage("Uploaded"+(int)progress+"%");
-                        }
-                    });
-        }
-    }
-
-    private void chooseImage() {
-        Intent intent=new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK
-                && data !=null && data.getData() !=null)
-        {
-            filePath=data.getData();
-            try{
-                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
-                imageView.setImageBitmap(bitmap);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
