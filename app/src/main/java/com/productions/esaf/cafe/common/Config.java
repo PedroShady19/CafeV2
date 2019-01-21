7c7bc5c38be711f1eb058187917727b8ade753a2
PedroShady19 <pmff.pedro19999@gmail.com>
2019-01-21 10:35:59 +0000

    commit 34.1 Atualizações nas dependências

---
+++ app/src/main/java/com/productions/esaf/cafe/common/Config.java
@@ -0,0 +1,6 @@
+package com.productions.esaf.cafe.common;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID="AZnw3gJsKntp41lKXZ0G6YKurPQOh11mY7BhN86CanwQMFyurJScWfnNfP85ijJmc_g8wI0Yml7uLsse";
+
+}
